59b2cd6acbdfb2fa96d4cc43d90bc663900a4b99
Sigmundur Mørkøre <sm@sigmundur.dev>
2020-06-25 01:10:44 +0100

    Start of Sapper blog

---
+++ sigmundur-blog/src/routes/_layout.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Nav from "../components/Nav.svelte";
+
+  export let segment;
+</script>
+
+<style>
+  main {
+    position: relative;
+    max-width: 56em;
+    background-color: white;
+    padding: 2em;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
+  footer {
+    text-align: center;
+  }
+</style>
+
+<header>
+  <h1>Sigmundur's Blog</h1>
+  <Nav {segment} />
+</header>
+
+<main>
+  <slot />
+</main>
+
+<footer>🇫🇴</footer>
---
+++ sigmundur-blog/src/routes/contact.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+  <title>Contact Me</title>
+</svelte:head>
+
+<h1>Contact!</h1>
+
+<a href="https://github.com/SigmundurMorkore">Follow Me On Github</a>
+
+<footer>
+  <h3>THIS IS THE FOOTER</h3>
+</footer>
